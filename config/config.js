a351123555353c8e48aac8f41f744f89f100b5e2
Denis Chakhalian <91376823+DenisChakhalian@users.noreply.github.com>
2024-09-19 22:13:45 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,13 +9,7 @@ const settings = {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: true,
-      // Убедитесь, что у вас есть CA сертификат. Если нет, закомментируйте следующую строку
-      ca: fs.existsSync(path.join('/home/rdndzdht/ssl/certs', 'rdndzdht_a2hosted_com_b3779_1e9e7_1758300200_643e4daf1bf111c2b740b0486caf423d.crt'))
-         ? fs.readFileSync(path.join('/home/rdndzdht/ssl/certs', 'rdndzdht_a2hosted_com_b3779_1e9e7_1758300200_643e4daf1bf111c2b740b0486caf423d.crt')).toString()
-         : undefined,
-      key: fs.readFileSync(path.join('/home/rdndzdht/ssl/keys', 'e35a7_1b5f3_dbc9229a95fa06cb8580598414e2597f.key')).toString(),
-      cert: fs.readFileSync(path.join('/home/rdndzdht/ssl/certs', 'rdndzdht_a2hosted_com_b3779_1e9e7_1758300200_643e4daf1bf111c2b740b0486caf423d.crt')).toString()
+      rejectUnauthorized: true
     }
   }
 };

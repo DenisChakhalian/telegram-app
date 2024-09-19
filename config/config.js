71452a47c2f98f09e9741387e301f2f3fc920744
Denis Chakhalian <91376823+DenisChakhalian@users.noreply.github.com>
2024-09-19 21:43:44 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const DB_URI = process.env.DB_URL;
 
-const settings= {
+const settings = {
   url: DB_URI,
   dialectOptions: {
     ssl: {
-      require: false
+      require: true,
+      rejectUnauthorized: true,
+      // Убедитесь, что у вас есть CA сертификат. Если нет, закомментируйте следующую строку
+      ca: fs.existsSync(path.join('/home/rdndzdht/ssl/certs', 'rdndzdht_a2hosted_com_b3779_1e9e7_1758300200_643e4daf1bf111c2b740b0486caf423d.crt'))
+         ? fs.readFileSync(path.join('/home/rdndzdht/ssl/certs', 'rdndzdht_a2hosted_com_b3779_1e9e7_1758300200_643e4daf1bf111c2b740b0486caf423d.crt')).toString()
+         : undefined,
+      key: fs.readFileSync(path.join('/home/rdndzdht/ssl/keys', 'e35a7_1b5f3_dbc9229a95fa06cb8580598414e2597f.key')).toString(),
+      cert: fs.readFileSync(path.join('/home/rdndzdht/ssl/certs', 'rdndzdht_a2hosted_com_b3779_1e9e7_1758300200_643e4daf1bf111c2b740b0486caf423d.crt')).toString()
     }
   }
 };
